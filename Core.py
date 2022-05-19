f7e717a9f06c5791d29b8c8f2ef7a3ebc7d70b00
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2022-05-19 14:19:05 -0700

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,1 +1,16 @@
 #import sys, getopt, argparse, urllib.request, ssl, os, warning
+
+import request
+import json
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
+
+class Core.py
+    """
+    A class for something
+    
+    Methods
+    ------
+    get(uri, param)
+      Send GET request
